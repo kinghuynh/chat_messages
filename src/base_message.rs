ca78838aefd5fa6896c4fa9b65fb64051038aefd
Kingston Huynh <139024820+kinghuynh@users.noreply.github.com>
2024-09-07 08:51:46 -0500

    add message type enum

--- src/base_message.rs
+++ src/base_message.rs
@@ -1,8 +1,15 @@
 use std::collections::HashMap;
 
+#[derive(Debug, PartialEq, Eq)]
+pub enum MessageType {
+    Human,
+    AI,
+    System,
+}
+
 pub trait BaseMessage {
     fn content(&self) -> &str;
-    fn message_type(&self) -> &str;
+    fn message_type(&self) -> MessageType;
     fn additional_kwargs(&self) -> &HashMap<String, String>;
     fn response_metadata(&self) -> &HashMap<String, String>;
     fn id(&self) -> Option<&str>;
