2914fd8c81890eac6eb6f3b5e5723adabb7eadeb
Kingston Huynh <139024820+kinghuynh@users.noreply.github.com>
2024-09-07 09:37:33 -0500

    change macro to use message type enum

--- src/define_message.rs
+++ src/define_message.rs
@@ -1,30 +1,42 @@
 #[macro_export]
 macro_rules! define_message {
-    ($message_type:ident, $message_enum_str:expr) => {
-        pub struct $message_type {
-            pub base: BaseMessageFields,
-        }
+    ($message_type_enum:expr) => {
+        paste::item! {
+            pub struct [<$message_type_enum Message>] {
+                pub base: BaseMessageFields,
+            }
 
-        impl $message_type {
-            pub fn new(content: &str) -> Self {
-                Self {
-                    base: BaseMessageFields {
-                        content: content.to_string(),
-                        example: false,
-                        additional_kwargs: HashMap::new(),
-                        response_metadata: HashMap::new(),
-                        id: None,
-                        name: None,
-                    },
+            impl [<$message_type_enum Message>] {
+                pub fn new(content: &str) -> Self {
+                    Self {
+                        base: BaseMessageFields {
+                            content: content.to_string(),
+                            example: false,
+                            additional_kwargs: std::collections::HashMap::new(),
+                            response_metadata: std::collections::HashMap::new(),
+                            id: None,
+                            name: None,
+                        }
+                    }
+                }
+
+                pub fn content(&self) -> &str {
+                    &self.base.content
+                }
+
+                pub fn message_type(&self) -> MessageType {
+                    $message_type_enum
                 }
             }
 
-            pub fn content(&self) -> &str {
-                &self.base.content
-            }
+            impl BaseMessage for [<$message_type_enum Message>] {
+                fn content(&self) -> &str {
+                    &self.base.content
+                }
 
-            pub fn message_type(&self) -> Result<MessageType, InvalidMessageTypeError> {
-                MessageType::try_from($message_enum_str)
+                fn message_type(&self) -> MessageType {
+                    $message_type_enum
+                }
             }
         }
     };
