1242539c8aad40eb082d1510d571334dba2b4eb9
Kingston Huynh <139024820+kinghuynh@users.noreply.github.com>
2024-09-07 11:09:38 -0500

    add ai and system messages

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ pub use base_message::BaseMessage;
 pub use base_message::BaseMessageFields;
 pub use base_message::MessageType;
 
-pub mod human_message;
-
 pub mod define_message;
+
+pub mod ai_message;
+pub mod human_message;
+pub mod system_message;
---
+++ src/system_message.rs
@@ -0,0 +1,4 @@
+use crate::base_message::{BaseMessage, BaseMessageFields, MessageType, MessageType::System};
+use crate::define_message;
+
+define_message!(System);
