c910c534afb81c0f73ffb1a7123142fc8c0913f6
Kingston Huynh <139024820+kinghuynh@users.noreply.github.com>
2024-09-07 09:28:54 -0500

    implement to from str and add message type error

--- src/base_message.rs
+++ src/base_message.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt};
 
 #[derive(Debug)]
 pub struct BaseMessageFields {
@@ -15,6 +15,28 @@ pub enum MessageType {
     Human,
     AI,
     System,
+}
+
+#[derive(Debug)]
+pub struct InvalidMessageTypeError;
+
+impl fmt::Display for InvalidMessageTypeError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Invalid message type")
+    }
+}
+
+impl TryFrom<&str> for MessageType {
+    type Error = InvalidMessageTypeError;
+
+    fn try_from(s: &str) -> Result<MessageType, InvalidMessageTypeError> {
+        match s {
+            "Human" => Ok(MessageType::Human),
+            "AI" => Ok(MessageType::AI),
+            "System" => Ok(MessageType::System),
+            _ => Err(InvalidMessageTypeError),
+        }
+    }
 }
 
 pub trait BaseMessage {
