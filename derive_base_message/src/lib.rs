78455fd610ad504d7560b1a636ae0a4beb971ab7
Kingston Huynh <139024820+kinghuynh@users.noreply.github.com>
2024-09-08 08:02:31 -0500

    import derive_macro

--- derive_base_message/src/lib.rs
+++ derive_base_message/src/lib.rs
@@ -1,13 +1,10 @@
+mod derive_macro;
 mod field;
 
+use derive_macro::derive_macro;
 use proc_macro::TokenStream;
-use quote::quote;
 
 #[proc_macro_derive(BaseMessage)]
 pub fn derive_base_message(input: TokenStream) -> TokenStream {
-    // let input = parse_macro_input!(input as DeriveInput);
-    // let _name = &input.ident;
-
-    let expanded = quote! { "" };
-    expanded.into()
+    derive_macro(input.into()).into()
 }
