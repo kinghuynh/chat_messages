b93defaa94846f97ee2bfc76525219df2a6ddb19
Kingston Huynh <139024820+kinghuynh@users.noreply.github.com>
2024-09-08 07:58:39 -0500

    add field helpers with tests

--- derive_base_message/src/lib.rs
+++ derive_base_message/src/lib.rs
@@ -1,11 +1,12 @@
+mod field;
+
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(BaseMessage)]
 pub fn derive_base_message(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-    let _name = &input.ident;
+    // let input = parse_macro_input!(input as DeriveInput);
+    // let _name = &input.ident;
 
     let expanded = quote! { "" };
     expanded.into()
